cc9ba43e4a7dd71ad552498af7659c84d0c4381e
FATIMAH BADMUS <95576634+Codedivah@users.noreply.github.com>
2023-07-01 22:01:28 +0100

    Update config.ts

--- src/constant/https/config.ts
+++ src/constant/https/config.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "https://chatter-socials-jzcyfsx2v-codedivah.vercel.app/";
+const BASE_URL = "https://chatter-socials-jzcyfsx2v-codedivah.vercel.app/api/";
 const DB_USER = "chatteruser";
 
 export { BASE_URL, DB_USER };
